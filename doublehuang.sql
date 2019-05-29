d689304b7dea30a43d8546623e9ef791b96f70bf
DoubleHuangg <33065183+DoubleHuangg@users.noreply.github.com>
2019-05-29 16:43:19 +0800

    Update doublehuang.sql

--- doublehuang.sql
+++ doublehuang.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : Temple
-Source Server Version : 80014
+Source Server         : root
+Source Server Version : 80015
 Source Host           : localhost:3306
-Source Database       : doublehuang
+Source Database       : ayu
 
 Target Server Type    : MYSQL
-Target Server Version : 80014
+Target Server Version : 80015
 File Encoding         : 65001
 
-Date: 2019-02-24 18:04:09
+Date: 2019-05-27 21:33:12
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,31 +21,42 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `bank_information`;
 CREATE TABLE `bank_information` (
   `id` int(11) DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `address` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `swift_code` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `country` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `code` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `branch_code` varchar(255) COLLATE utf8_bin DEFAULT NULL
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `swift_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `country` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `code` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `branch_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Records of bank_information
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for customer
 -- ----------------------------
 DROP TABLE IF EXISTS `customer`;
 CREATE TABLE `customer` (
-  `id` int(11) NOT NULL,
-  `created_by` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `created_date` datetime DEFAULT NULL,
-  `last_modified_by` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `last_modified_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `last_modified_date` datetime DEFAULT NULL,
-  `company` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `country` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `region` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `company` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `country` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `region` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `telphone` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Records of customer
+-- ----------------------------
+INSERT INTO `customer` VALUES ('1', '1', null, null, null, 'Toyop Relief PVT. LTD', 'USA', 'procurement@toyop.com', 'Varsha Desai', '', '601/602, 6th Floor, Presidental Plaza, Opp R City Mall, Ghatkopar West, Mumbai – 400086', ' +91-22-67590000/25771647');
 
 -- ----------------------------
 -- Table structure for declare_customs
@@ -53,12 +64,16 @@ CREATE TABLE `customer` (
 DROP TABLE IF EXISTS `declare_customs`;
 CREATE TABLE `declare_customs` (
   `id` int(11) NOT NULL,
-  `bill` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `contract` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `customs_list` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `invoice` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `bill` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `contract` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `customs_list` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `invoice` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Records of declare_customs
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for logistics
@@ -66,47 +81,78 @@ CREATE TABLE `declare_customs` (
 DROP TABLE IF EXISTS `logistics`;
 CREATE TABLE `logistics` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `odd_numbers` int(11) DEFAULT NULL,
-  `voucher` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `voucher` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Records of logistics
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for order
 -- ----------------------------
 DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order` (
-  `code` varchar(255) COLLATE utf8_bin NOT NULL,
+  `id` int(11) NOT NULL,
+  `code` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `user_id` int(255) DEFAULT NULL,
   `customer_id` int(255) DEFAULT NULL,
-  `advance_charge_list` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `purchase_orde_list` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `shipment_list` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `advance_charge_list` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `purchase_orde_list` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `shipment_list` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `transport_id` int(11) DEFAULT NULL,
   `declare_customs_id` int(11) DEFAULT NULL,
   `logistics_id` int(11) DEFAULT NULL,
   `payment_term` int(11) DEFAULT NULL,
   `price_basis` int(11) DEFAULT NULL,
-  `remarks` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `remarks` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `advance_charges` int(255) DEFAULT NULL,
   `tail_charges` int(255) DEFAULT NULL,
   `created_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `created_date` datetime DEFAULT NULL,
   `last_modified_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `last_modified_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`code`)
+  `product_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Records of order
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for product
+-- ----------------------------
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `price_basis` int(11) DEFAULT NULL,
+  `model` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Records of product
+-- ----------------------------
+INSERT INTO `product` VALUES ('1', '红', '48', 'MD-088');
 
 -- ----------------------------
 -- Table structure for test_table
 -- ----------------------------
 DROP TABLE IF EXISTS `test_table`;
 CREATE TABLE `test_table` (
-  `student` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `subject` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `student` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `subject` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `score` int(22) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Records of test_table
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for transport
@@ -115,29 +161,37 @@ DROP TABLE IF EXISTS `transport`;
 CREATE TABLE `transport` (
   `id` int(11) NOT NULL,
   `freight` int(11) DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `odd_numbers` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Records of transport
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `created_by` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `created_date` datetime DEFAULT NULL,
-  `last_modified_by` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `last_modified_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `last_modified_date` datetime DEFAULT NULL,
-  `department` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `phone` int(11) DEFAULT NULL,
+  `department` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `phone` varchar(11) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', null, null, null, null, 'business', '123@qq.com', '何钰恒', '123', '12345678910');
 
 -- ----------------------------
 -- Table structure for user_customer_mapping
@@ -149,6 +203,10 @@ CREATE TABLE `user_customer_mapping` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
+-- Records of user_customer_mapping
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for user_order_mapping
 -- ----------------------------
 DROP TABLE IF EXISTS `user_order_mapping`;
@@ -158,3 +216,6 @@ CREATE TABLE `user_order_mapping` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+-- ----------------------------
+-- Records of user_order_mapping
+-- ----------------------------
